6629f5c8323ba0b53c76831e1bab56fff8531579
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-09-02 15:35:18 +0430

    i did it! custum usercreateform!

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import MyUserCreationForm
 from django.contrib.auth.views import LoginView
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
@@ -19,7 +19,7 @@ class CustomLoginView(LoginView):
 
 class RegisterPage(FormView):
     template_name = "accounts/register.html"
-    form_class = UserCreationForm
+    form_class = MyUserCreationForm
     redirect_authenticated_user = True
     success_url = reverse_lazy("task_list")
 
